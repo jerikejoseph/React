1439da795190324749304d21503c75178d08d682
jerikejoseph <jerikejoseph144@gmail.com>
2024-10-31 13:24:43 +0530

    asd

---
+++ src/components/Labels.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Labels extends React.Component{
+    render(){
+        const {isActive}=this.props;
+        const style=isActive?{background}
+        return(
+     <span>label 1</span>
+        )
+    }
+}
+
+export default Labels;
